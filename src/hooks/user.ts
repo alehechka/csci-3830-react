a4ce8f2aaf7026bbeee7b779c28e2d65eb77651a
Adam Lehechka <42357034+alehechka@users.noreply.github.com>
2020-12-04 22:38:16 -0600

    Hooks and functions

---
+++ src/hooks/user.ts
@@ -0,0 +1,32 @@
+import { useCreateGeneric, useDeleteGeneric, useUpdateGeneric } from './generics';
+import { useCollection, useDocument } from './queries';
+import { User } from '../models';
+import { useAuth } from './auth';
+
+export const useCreateUser = () => {
+	const createGeneric = useCreateGeneric<User>();
+	return (user: User) => createGeneric('users', user);
+};
+
+export const useUpdateUser = () => {
+	const updateGeneric = useUpdateGeneric<User>();
+	return (userUID: string, user: User) => updateGeneric('users', userUID, user);
+};
+
+export const useDeleteUser = () => {
+	const deleteGeneric = useDeleteGeneric();
+	return (userUID: string) => deleteGeneric('users', userUID);
+};
+
+export const useSelectedUser = (userUID: string) => {
+	return useDocument<User>('users', userUID);
+};
+
+export const useUsers = () => {
+	return useCollection<User>('users');
+};
+
+export const useCurrentUser = () => {
+	const [auth] = useAuth();
+	return useSelectedUser(auth.uid);
+};
---
+++ src/models/User.ts
@@ -0,0 +1,9 @@
+export interface User {
+	firstName?: string;
+	lastName?: string;
+	color?: string;
+	number?: number;
+	major?: string;
+	title?: string;
+	admin: boolean;
+}
